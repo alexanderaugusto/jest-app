6e7da62b9c5cf0efc8b376329d4039675bc35036
Alexander Augusto <alexaasf_10@hotmail.com>
2020-10-11 09:40:12 -0300

    change num1 to num

--- src/AdvancedCalculator.js
+++ src/AdvancedCalculator.js
@@ -1,18 +1,18 @@
 class AdvancedCalculator {
 
-    abs(num1) {
-        return Math.abs(num1)
+    abs(num) {
+        return Math.abs(num)
     }
 
     pow(num1, num2) {
         return Math.pow(num1, num2)
     }
 
-    sqrt(num1) {
-        if (num1 < 0) {
+    sqrt(num) {
+        if (num < 0) {
             throw 'Indefinido'
         }
-        return Math.sqrt(num1)
+        return Math.sqrt(num)
     }
 }
 
